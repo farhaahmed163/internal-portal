e29944a34f14b53f2bb3d992905bc6dee14a155a
Farha Ahmed <farhaa853@gmail.com>
2024-05-22 14:23:45 +0300

    handle popup

---
+++ src/app/core/services/toast.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from 'primeng/api';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  constructor(private messageService: MessageService) {}
+
+  showSuccess(content: string) {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Message Content',
+    });
+  }
+
+  showInfo() {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Info',
+      detail: 'Message Content',
+    });
+  }
+
+  showWarn(content: string) {
+    this.messageService.add({
+      severity: 'warn',
+      summary: 'Warn',
+      detail: content,
+    });
+  }
+
+  showError(content: string) {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: content,
+    });
+  }
+
+  showContrast() {
+    this.messageService.add({
+      severity: 'contrast',
+      summary: 'Error',
+      detail: 'Message Content',
+    });
+  }
+
+  showSecondary() {
+    this.messageService.add({
+      severity: 'secondary',
+      summary: 'Secondary',
+      detail: 'Message Content',
+    });
+  }
+}
